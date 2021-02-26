3d5784d8dbac8efe778fecd3c6f8a499ab175a39
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-26 15:31:15 +0530

    starting with project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			{/* start of header */}
+			<Header />
+			{/* end of header */}
+
+			{/* start of cards */}
+
+			{/* end of cards */}
+
+			{/* start of swipe buttons */}
+
+			{/* end of swipe buttons */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import '../stylesheets/Header.css';
+const Header = () => {
+	return (
+		<div className='Heading'>
+			<h2>I'm a Header</h2>
+		</div>
+	);
+};
+
+export default Header;
